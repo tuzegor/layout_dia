64514c49dac1926c1fd1213b7cd15a351b24a334
Yehor Tuz <tuzegor@gmail.com>
2022-12-24 23:52:37 +0200

    fixed bugs, moved block social-media-icons, fixed structure and added region in html

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const userData = {
+    userEmail: contactForm.querySelector('[name="user-email"]').value,
+    userName: contactForm.querySelector('[name="user-name"]').value,
+    userComment: contactForm.querySelector('[name="user-message"]').value,
+  };
+
+  window.alert(`Thank you ${userData.userName}, we will contact you!`);
+  contactForm.reset();
+});
